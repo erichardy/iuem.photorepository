786b358e6df424c16d49bdcf9e3c9efcb410f9b5
Eric Hardy <Eric.Hardy@univ-brest.fr>
2012-05-07 18:10:44 +0200

    ManageMetadata_form in progress

--- iuem/photorepository/browser/ManageMetadata_form.py
+++ iuem/photorepository/browser/ManageMetadata_form.py
@@ -2,29 +2,18 @@ from five import grok
 from plone.directives import form
 
 from zope import schema
-from z3c.form import button
+from z3c.form import button , field
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from Products.statusmessages.interfaces import IStatusMessage
 from zope.component import adapts
 from Products.ATContentTypes.interface import IATFolder
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary
 from iuem.photorepository import iuemMessageFactory as _
-from plone.autoform.form import AutoExtensibleForm
+from Products.AddRemoveWidget import AddRemoveWidget
 from plone.z3cform import layout
 from zope.component import getUtility
 from plone.i18n.normalizer.interfaces import INormalizer
-
-@grok.provider(IContextSourceBinder)
-def possibleWhere(context):
-    w = []
-    # import pdb;pdb.set_trace()
-    
-    for ww in context['where']:
-        normalizer = getUtility(INormalizer)
-        nww = normalizer.normalize(unicode(ww , 'utf-8'), locale = 'fr')
-        uww = unicode(ww , 'utf-8')
-        w.append(SimpleVocabulary.createTerm(nww,nww,uww))   
-    return SimpleVocabulary(w)
 
 class metadataSource(object):
     grok.implements(IContextSourceBinder)
@@ -38,6 +27,12 @@ class metadataSource(object):
             uww = unicode(ww , 'utf-8')
             w.append(SimpleVocabulary.createTerm(nww,nww,uww))   
         return SimpleVocabulary(w)
+class stdMetadataSource(object):
+    grok.implements(IContextSourceBinder)
+    def __init__(self,k):
+        self.k = k
+    def __call__(self , context):
+        return SimpleVocabulary
 
 class IManageMetadataForm(form.schema.Schema):
     """metadata form"""
@@ -46,17 +41,36 @@ class IManageMetadataForm(form.schema.Schema):
     autor = schema.ASCIILine(title=u"Auteur",
                              description=u"a person")
     """
-    whereToSpread = schema.Choice(title=u"Where to spread",
-                                  description=u"Description where to spread",
-                                  values=['only Images','everywhere'])
-    where = schema.Set(title=u"Localisation",
-                                  description=_(u"Localisations"),
-                                  value_type=schema.Choice(source=possibleWhere)
-                                  )
-    laboratory =schema.Set(title=u"Laboratory",
-                                  description=_(u"Localisations"),
-                                  value_type=schema.Choice(source=metadataSource('laboratory'))
-                                  )
+    whereToSpread  = schema.Choice(title=u"Where to spread",
+                     required=False,
+                     description=u"Description where to spread",
+                     values=['only Images','everywhere']
+                     )
+    addOrReplace   = schema.Choice(title=_(u"addOrPreplace"),
+                     required=False,
+                     description=_(u"addOrReplaceDesc"),
+                     values=['add','replace'],
+                     default='add'
+                     )
+    xdescription   = schema.Text(title=u"Description",
+                     required=False,
+                     description=_(u"Description")
+                     )
+    where          = schema.Set(title=u"Localisation",
+                     required=False,
+                     description=_(u"Localisations"),
+                     value_type=schema.Choice(source=metadataSource('where'))
+                     )
+    laboratory     = schema.Set(title=u"Laboratory",
+                     required=False,
+                     description=_(u"Laboratories"),
+                     value_type=schema.Choice(source=metadataSource('laboratory'))
+                     )
+    reseachproject = schema.Set(title=u"reseachproject",
+                     required=False,
+                     description=_(u"reseachproject"),
+                     value_type=schema.Choice(source=metadataSource('reseachproject'))
+                     )
 
 class ManageMetadataForm(form.form.SchemaForm):
     """The form"""
@@ -69,29 +83,41 @@ class ManageMetadataForm(form.form.SchemaForm):
     
     label = u"distribute metadata values among objects"
     description = u"Decide where to spread metadatas"
-    
-    # import pdb;pdb.set_trace()
+    fields = field.Fields(IManageMetadataForm)
+    fields['where'].widgetFactory = CheckBoxFieldWidget
+    fields['laboratory'].widgetFactory = CheckBoxFieldWidget
+    fields['reseachproject'].widgetFactory = CheckBoxFieldWidget
+    # print dir(fields['xdescription'])
+    # fields['xdescription'].ignoreContext = False
 
     def getContent(self):
         context = self.context
-        # import pdb;pdb.set_trace()
-        x = 'retour de getContent()'
         data = {}
-        # import pdb;pdb.set_trace()
+        data['xdescription'] = context.Description()
         data['where'] = context.where
         data['laboratory'] = context.laboratory
-        
+        data['reseachproject'] = context.reseachproject
+        print context.Description()
         return data
-    
-    @button.buttonAndHandler(u'Ok')
+        
+    @button.buttonAndHandler(u'Ok',accessKey=u"o")
     def handleOk(self, action):
         data, errors = self.extractData()
-        
+        print data
+        # print '============================'
+        # print errors
         if errors:
             self.status = self.formErrorsMessage
             return
-    
-        
+        request = self.request
+        # nextUrl = '%s/@@metadata_view_pt'%self.context.absolute_url()
+        nextUrl = self.context.absolute_url()
+        request.response.redirect(nextUrl)
+
+
+@form.default_value(field=IManageMetadataForm['where'])
+def defWhere(data):
+    return ['plouzane']
         
 ManageMetadataFormView = layout.wrap_form(ManageMetadataForm)    
 
---
+++ iuem/photorepository/static/js/metadataForm.js
@@ -0,0 +1,23 @@
+
+
+jq(document).ready(function() { 
+ jq('#formfield-form-widgets-where input').attr('checked', true);
+ });
+
+jq(document).ready(function() { 
+	 jq('#formfield-form-widgets-laboratory input').attr('checked', true);
+	 });
+
+jq(document).ready(function() { 
+	 jq('#formfield-form-widgets-reseachproject input').attr('checked', true);
+	 });
+
+/*
+jq(document).ready(function() { 
+	 jq('#image-base-edit input[name=form.button.save]').click(function() {
+	   alert('Apres le load... ' + (jq('#where_selected').val()) );
+	   jq("#context").load("UpdateVocs");
+	 });
+	});
+
+*/
