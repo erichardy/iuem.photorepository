3f085a33257bc8b252dfefb0a801db149978a53b
Eric Hardy <Eric.Hardy@univ-brest.fr>
2012-05-10 17:23:14 +0200

    ManageMetadata in progress

--- iuem/photorepository/browser/MetadataConfirm.py
+++ iuem/photorepository/browser/MetadataConfirm.py
@@ -5,15 +5,50 @@ from Products.CMFCore.utils import getToolByName
 
 
 class MetadataConfirmView(BrowserView):
-    # import pdb;pdb.set_trace()
-
+    
+    def whereToSpread(self):
+        context = self.context
+        return context.whereToSpread
+    
+    def addOrReplace(self):
+        context = self.context
+        return context.addOrReplace
+    
+    def description(self):
+        context = self.context
+        return context.description
+    
     def general(self):
         context = self.context
         return context.general
-
+    
+    def science(self):
+        context = self.context
+        return context.science
+    
     def where(self):
         context = self.context
         return context.where
+    
+    def laboratory(self):
+        context = self.context
+        return context.laboratory
+    
+    def reseachproject(self):
+        context = self.context
+        return context.reseachproject
+    
+    def licencetype(self):
+        context = self.context
+        return context.licencetype
+    
+    def recording_date_time(self):
+        context = self.context
+        return context.recording_date_time
+    
+    def photographer(self):
+        context = self.context
+        return context.photographer
 
 
 class SpreadMetadata(BrowserView):
@@ -23,9 +58,19 @@ class SpreadMetadata(BrowserView):
         request = self.request
         portal = context.portal_url.getPortalObject()
         catalog = getToolByName(portal , 'portal_catalog')
+        query = {}
+        query['path'] = {'query':'/'.join(context.getPhysicalPath()) , 'depth':1}
+        query['portal_type'] = 'Image'
+        results = catalog(query)
+        # import pdb;pdb.set_trace()
         # http://plone.org/documentation/manual/developer-manual/indexing-and-searching/querying-the-catalog
-        print "in Spread Metadata...."
-        print request.form
-        print '----'
-        print catalog
-        print "\n"
+        for brains in results:
+            print brains.getObject().getId()
+            print brains.getObject().Description()
+            obj = brains.getObject()
+            print obj['where']
+        # print "in Spread Metadata...."
+        # print request.form
+        # print '----'
+        # print catalog
+        # print "\n"
