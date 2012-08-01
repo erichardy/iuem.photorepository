cb636718d97dc764d83f075446c62e56a4b7a350
Eric Hardy <Eric.Hardy@univ-brest.fr>
2012-08-01 16:19:07 +0200

    README.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1a3.dev0'
 setup(name='iuem.photorepository',
       version=version,
       description="Addon for Plone",
-      long_description=open("README.rst").read() + "\n" +
+      long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
